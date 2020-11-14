5c46d922400d2bc47c4fa6b8f4930141150aba13
Hero0011 <49434715+Hero0011@users.noreply.github.com>
2020-11-14 15:26:59 +1100

    Update zigphy-tx.c

--- zigphy-tx.c
+++ zigphy-tx.c
@@ -4,6 +4,8 @@
 #include "dev/watchdog.h"
 #include "sys/clock.h"
 #include <stdio.h>
+#include <string.h>
+#include "ieee-addr.h"
 
 static unsigned char packet[64] = {0};
 
@@ -20,10 +22,15 @@ PROCESS_THREAD(zigphy_tx_process, ev, data)
 	//value = cc2420_get_txpower();
 	NETSTACK_RADIO.get_value(RADIO_PARAM_TXPOWER, &value);
 	printf(" TXPWR: %d dBm\n", value);
+	uint8_t addr[8];
+	ieee_addr_cpy_to(addr, 8);
+	for (int i = 0; i < 8; ++i) {
+		sprintf(packet, "%s%02X", packet, addr[i]);
+	}
 	while(1) {
 		if (++i % 1000 == 0) printf("sending %d\n", i);
 		NETSTACK_RADIO.send(packet, 64);
-		//clock_wait(CLOCK_SECOND); //delay between packets
+		clock_wait(CLOCK_SECOND); //delay between packets
 	}
 	PROCESS_END();
 }
